f93fcd752fee3dbcd004d79f571a6758e669f834
K213-UX <413061369@qq.com>
2026-04-15 13:37:51 +0800

    docs: update README.md

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 5173,
-    open: true
+    open: true,
+    host: true
+  },
+  preview: {
+    allowedHosts: ['www.zhichuangkeji.online']
   }
 })
